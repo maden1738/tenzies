60ac599be9c61f1db1c20550f951a85659c1f94b
Swarna Jang Maden <limbujungmaden@gmail.com>
2024-01-10 16:08:22 +0545

    add dots instead of number in die

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import Dot from "./Dot";
 
 export default function Die(props) {
        const styles = {
               backgroundColor: props.isHeld ? "#59E391" : "white",
        };
        return (
-              <div className="die-face" style={styles} onClick={props.holdDice}>
-                     <h2 className="die-num">{props.value}</h2>
+              <div
+                     className={`die-face die-${props.value}`}
+                     style={styles}
+                     onClick={props.holdDice}
+              >
+                     {/* <h2 className="die-num">{props.value}</h2> */}
+                     {Array(props.value)
+                            .fill(0)
+                            .map((el) => (
+                                   <Dot />
+                            ))}
               </div>
        );
 }
