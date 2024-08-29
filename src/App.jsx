ab9c76606364ea5e2ff9969ab1b5aad217c36409
swarnaMaden <swarnamaden@lftechnology.com>
2024-08-29 15:30:39 +0545

    fix timer reset

--- src/App.jsx
+++ src/App.jsx
@@ -4,115 +4,112 @@ import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
 
 export default function App() {
-       const [dice, setDice] = React.useState(allNewDice());
-       const [tenzies, setTenzies] = React.useState(false);
+  const [dice, setDice] = React.useState(allNewDice());
+  const [tenzies, setTenzies] = React.useState(false);
 
-       const [time, setTime] = React.useState({
-              startTime: null,
-              endTime: null,
-       });
-       const [totalTime, setTotalTime] = React.useState();
-       const [highScore, setHighScore] = React.useState(
-              localStorage.getItem("highScore") || 9999999
-       );
+  const [time, setTime] = React.useState({
+    startTime: null,
+    endTime: null,
+  });
+  const [totalTime, setTotalTime] = React.useState();
+  const [highScore, setHighScore] = React.useState(
+    localStorage.getItem("highScore") || 9999999
+  );
 
-       React.useEffect(() => {
-              const allHeld = dice.every((die) => die.isHeld);
-              const firstValue = dice[0].value;
-              const allSameValue = dice.every(
-                     (die) => die.value === firstValue
-              );
-              if (allHeld && allSameValue) {
-                     setTime({ ...time, endTime: new Date() }); // if game is over set the endTime
-                     setTenzies(true);
-              }
-       }, [dice]);
+  React.useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((die) => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTime({ ...time, endTime: new Date() }); // if game is over set the endTime
+      setTenzies(true);
+    }
+  }, [dice]);
 
-       React.useEffect(() => {
-              if (time.startTime && time.endTime) {
-                     calculateTimeTaken();
-              }
-       }, [time]); // if both startTime and endTime is set, calculate total time
+  React.useEffect(() => {
+    if (time.startTime && time.endTime) {
+      calculateTimeTaken();
+    }
+  }, [time]); // if both startTime and endTime is set, calculate total time
 
-       React.useEffect(() => {
-              if (totalTime < highScore) {
-                     localStorage.setItem("highScore", totalTime);
-                     setHighScore(totalTime);
-              }
-       }, [totalTime]); // check for highscore after totalTime is set
+  React.useEffect(() => {
+    if (totalTime < highScore) {
+      localStorage.setItem("highScore", totalTime);
+      setHighScore(totalTime);
+    }
+  }, [totalTime]); // check for highscore after totalTime is set
 
-       function calculateTimeTaken() {
-              const timeTaken = time.endTime - time.startTime;
-              setTotalTime(timeTaken / 1000);
-       }
+  function calculateTimeTaken() {
+    const timeTaken = time.endTime - time.startTime;
+    setTotalTime(timeTaken / 1000);
+  }
 
-       function generateNewDie() {
-              return {
-                     value: Math.ceil(Math.random() * 6),
-                     isHeld: false,
-                     id: nanoid(),
-              };
-       }
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
 
-       function allNewDice() {
-              const newDice = [];
-              for (let i = 0; i < 10; i++) {
-                     newDice.push(generateNewDie());
-              }
-              return newDice;
-       }
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie());
+    }
+    return newDice;
+  }
 
-       function rollDice() {
-              if (!tenzies) {
-                     setDice((oldDice) =>
-                            oldDice.map((die) => {
-                                   return die.isHeld ? die : generateNewDie();
-                            })
-                     );
-              } else {
-                     setTenzies(false);
-                     setDice(allNewDice());
-              }
-       }
+  function rollDice() {
+    if (!tenzies) {
+      setDice((oldDice) =>
+        oldDice.map((die) => {
+          return die.isHeld ? die : generateNewDie();
+        })
+      );
+    } else {
+      setTime({ startTime: null, endTime: null });
+      setTenzies(false);
+      setDice(allNewDice());
+    }
+  }
 
-       function holdDice(id) {
-              !time.startTime && setTime({ ...time, startTime: new Date() }); // setting startTime only when first die is hold
-              setDice((oldDice) =>
-                     oldDice.map((die) => {
-                            return die.id === id
-                                   ? { ...die, isHeld: !die.isHeld }
-                                   : die;
-                     })
-              );
-       }
+  function holdDice(id) {
+    !time.startTime && setTime({ ...time, startTime: new Date() }); // setting startTime only when first die is hold
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
 
-       const diceElements = dice.map((die) => (
-              <Die
-                     key={die.id}
-                     value={die.value}
-                     isHeld={die.isHeld}
-                     holdDice={() => holdDice(die.id)}
-              />
-       ));
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
 
-       return (
-              <main>
-                     {tenzies && <Confetti />}
-                     <h1 className="title">Tenzies</h1>
-                     <p className="instructions">
-                            Roll until all dice are the same. Click each die to
-                            freeze it at its current value between rolls.
-                     </p>
-                     <div className="dice-container">{diceElements}</div>
-                     <button className="roll-dice" onClick={rollDice}>
-                            {tenzies ? "New Game" : "Roll"}
-                     </button>
-                     {tenzies && (
-                            <>
-                                   <p>You completed in {totalTime} seconds</p>
-                                   <p>The high score is {highScore} seconds</p>
-                            </>
-                     )}
-              </main>
-       );
+  return (
+    <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        {tenzies ? "New Game" : "Roll"}
+      </button>
+      {tenzies && (
+        <>
+          <p>You completed in {totalTime} seconds</p>
+          <p>The high score is {highScore} seconds</p>
+        </>
+      )}
+    </main>
+  );
 }
